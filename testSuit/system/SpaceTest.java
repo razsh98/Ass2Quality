31ddee36a543a50f37053aa629a5d2d26636e008
razsh98 <44240102+razsh98@users.noreply.github.com>
2019-12-27 16:45:48 +0200

    Add files via upload
    Initial Commit

---
+++ testSuit/system/SpaceTest.java
@@ -0,0 +1,31 @@
+package system;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpaceTest {
+
+//    @Before
+//    public void newDummySpace(){
+//
+//    }
+
+    @Test
+    public void alloc() {
+
+    }
+
+    @Test
+    public void dealloc() {
+    }
+
+    @Test
+    public void countFreeSpace() {
+    }
+
+    @Test
+    public void getAlloc() {
+    }
+}
